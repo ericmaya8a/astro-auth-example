450b9faad8929aacb3a1449c1a2672d367df59a9
Eric Maya <ericmaya8a@gmail.com>
2024-01-26 12:56:27 -0600

    chore: update styles

--- src/components/react/Input.tsx
+++ src/components/react/Input.tsx
@@ -5,8 +5,8 @@ type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 export function Input(props: InputProps) {
   return (
     <div style={props.style}>
-      <label className="mb-1 block" htmlFor={props.id}>
-        {props["aria-label"]}{" "}
+      <label className="mb-1 flex gap-1" htmlFor={props.id}>
+        <span>{props["aria-label"]}</span>
         {props.required ? <span className="text-red-700">*</span> : null}
       </label>
       <input className="input w-full" {...props} />
