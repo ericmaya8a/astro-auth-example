6098f47c8ae1c3e06e27157cb4ad7ce284963dfd
Eric Maya <ericmaya8a@gmail.com>
2024-01-27 15:16:11 -0600

    chore: add createUser

--- src/server/modules/user.ts
+++ src/server/modules/user.ts
@@ -1,5 +1,12 @@
 import { prisma } from "../../../db/client";
+import type { RegisterSchemaT } from "../../schemas";
 
 export async function getUserByEmail(email: string) {
   return await prisma.user.findUnique({ where: { email } });
+}
+
+export async function createUser(user: RegisterSchemaT) {
+  return await prisma.user.create({
+    data: user,
+  });
 }
